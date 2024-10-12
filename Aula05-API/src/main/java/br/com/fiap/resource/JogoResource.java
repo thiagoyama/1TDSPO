14107335e4cb04c1c35a010d1d2f47f645dc04d9
Thiago Yamamoto <thiagoyama@gmail.com>
2024-10-11 22:32:00 -0300

    API

--- Aula05-API/src/main/java/br/com/fiap/resource/JogoResource.java
+++ Aula05-API/src/main/java/br/com/fiap/resource/JogoResource.java
@@ -40,4 +40,38 @@ public class JogoResource {
         return jogoDao.pesquisarPorId(id);
     }
 
+    @PUT
+    @Path("/{id}")
+    public Response atualizar(@PathParam("id") int id, Jogo jogo) throws IdNaoEncontradoException, SQLException {
+        //Setar o id no jogo
+        jogo.setId(id);
+        //Atualizar o jogo
+        jogoDao.atualizar(jogo);
+        //Retornar o status 200 OK
+        return Response.ok().build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response remover(@PathParam("id") int id) throws IdNaoEncontradoException, SQLException {
+        jogoDao.remover(id);
+        return Response.noContent().build();
+    }
+
+    @PATCH //Realizar a atualização parcial do objeto
+    @Path("/{id}")
+    public Response atualizarParcial(@PathParam("id") int id, Jogo jogo) throws IdNaoEncontradoException, SQLException {
+        //Pesquisar o jogo no banco de dados
+        Jogo jogoBanco = jogoDao.pesquisarPorId(id);
+        //Verifica se existe valor no objeto recebido, eu seto o novo valor no objeto do banco
+        if (jogo.getNome() != null)
+            jogoBanco.setNome(jogo.getNome());
+        if (jogo.getClassificacao() != null)
+            jogoBanco.setClassificacao(jogo.getClassificacao());
+        if (jogo.getDataLancamento() != null)
+            jogoBanco.setDataLancamento(jogo.getDataLancamento());
+        jogoDao.atualizar(jogoBanco);
+        return Response.ok().build();
+    }
+
 }
